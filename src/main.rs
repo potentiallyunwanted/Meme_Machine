1a20fe63a403f3e9adc44acc5709ad6496fc61d3
Quoc Duong <quoc.duong@potentiallyunwanted>
2022-04-07 18:42:53 -0700

    app functionality complete

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,53 @@
-use std::io;
+use std::{io, path::Path};
+use image::Rgba;
+use imageproc::drawing::draw_text_mut;
+use rusttype::{Font, Scale};
+
 
 fn main() {
     struct Meme{
         name: String,
-        text: String
+        text_top: String,
+        text_bottom: String,
+        image_path: String
     }
 
-    let will_slap = Meme {
-        name: String::from("Will Smith Slapping Chris Rock"),
-        text: String::new()
+    let mut one_does_not = Meme {
+        name: String::from("One Does Not Simply"),
+        text_top: String::from("One Does Not Simply"),
+        text_bottom: String::new(),
+        image_path: String::from("/home/qduong/rust_projects/meme_machine/src/one_does_not.png")
     };
 
-    let two_button = Meme {
-        name: "Two Buttons".to_string(),
-        text: String::new()
+fn make_meme(meme: &Meme) {
+
+    let path = Path::new(&meme.image_path);
+    let mut meme_image = image::open(path).unwrap();
+
+    let font = Vec::from(include_bytes!("DejaVuSans.ttf") as &[u8]);
+    let font = Font::try_from_vec(font).unwrap();
+
+    let height = 25.0;
+    let scale = Scale {
+        x: height * 2.0,
+        y: height,
     };
+
+    let color = Rgba([255u8, 255u8, 255u8, 0u8]);
+
+    draw_text_mut(&mut meme_image, color, 40, 0, scale, &font, &meme.text_top);
+    draw_text_mut(&mut meme_image, color, 40, 250, scale, &font, &meme.text_bottom);
+    let _ = meme_image.save(Path::new("new_meme.png")).unwrap();
+
+    println!("Your meme has been created.");
+}
+
 
     const APP_TITLE: &str = "Meme Machine";
     println!("This App is called {}", APP_TITLE);
 
     println!("Pick the meme you want to make:");
-    println!("1. {}", will_slap.name);
-    println!("2. {}", two_button.name);
+    println!("1. {}", one_does_not.name);
     
     let mut pick = String::new();
 
@@ -32,9 +58,19 @@ fn main() {
     let pick: u32 = pick.trim().parse().expect("Please type a number!");
 
     if pick == 1{
-        println!("You picked the Will Smith Slapping Chris Rock Meme")
-    }else if pick == 2{
-        println!("You picked the Two Buttons Meme")
+        println!("You picked the One Does Not Simply...");
+        println!("What is your bottom text going to be?");
+
+        let mut bottom = String::new();
+
+        io::stdin()
+            .read_line(&mut bottom)
+            .expect("Failed to read line");
+        
+        one_does_not.text_bottom = bottom.trim().to_string();
+        
+        make_meme(&one_does_not);
+
     }else{
         println!("Please pick 1 or 2. Thanks.")
     }
